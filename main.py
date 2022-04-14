a73e8e7c8185e94f5368067d367ec7311f296208
Jenny-Tan <55517646+Jenny-Tan@users.noreply.github.com>
2022-04-14 17:57:19 -0400

    update

--- main.py
+++ main.py
@@ -3,10 +3,47 @@ from matplotlib.pyplot import axis
 import numpy as np
 import pandas as pd
 import pickle
+import torch
+import torch.nn as nn
 
-<<<<<<< Updated upstream
-with open('./kreg.pkl', 'rb') as f:
-    model = pickle.load(f)
+class Dense(nn.Module):
+    def __init__(self):
+        super(Dense, self).__init__()
+        self.encoder = nn.Sequential(
+            nn.Linear(600, 400),
+            nn.LeakyReLU(),
+            nn.Dropout(p=0.2),
+            nn.Linear(400, 300),
+            nn.LeakyReLU(),
+            nn.Dropout(p=0.2),
+            nn.Linear(300, 100)
+        )
+        self.decoder = nn.Sequential(
+            nn.Linear(100, 300),
+            nn.LeakyReLU(),
+            nn.Dropout(p=0.2),
+            nn.Linear(300, 400),
+            nn.LeakyReLU(),
+            nn.Dropout(p=0.2),
+            nn.Linear(400, 600),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        for l in self.encoder:
+            x = l(x)
+        for l in self.decoder:
+            x = l(x)
+        return x
+
+model = Dense()
+model.load_state_dict(torch.load("bottle.neck"), strict=False)
+model.eval()
+
+reg = pickle.load(open("LinearReg.pkl", 'rb'))
+
+pr_scaler = pickle.load(open("pr_scaler.pkl", 'rb'))
+vo_scaler = pickle.load(open("vo_scaler.pkl", 'rb'))
 
 def get_r_hat(A, B): 
     """
@@ -14,11 +51,19 @@ def get_r_hat(A, B):
         B: 1440-by-10 dataframe of trading volumes with columns volu_0, ... , volu_9    
         return: a numpy array of length 10, corresponding to the predictions for the forward 30-minutes returns of assets 0, 1, 2, ..., 9
     """
-    X = A.iloc[-50:].values
-    X = np.concatenate([X, X[1:] - X[:-1]], axis=0)
-    pred = model.predict(X.reshape(1, -1))
-    return pred # Use the negative 30-minutes backward log-returns to predict the 30-minutes forward log-returns
-    
-=======
-     return np.array([m.predict(input)[0] for m in model]) - A[:,-1]
->>>>>>> Stashed changes
+    # X = A.iloc[-50:].values
+    # X = np.concatenate([X, X[1:] - X[:-1]], axis=0)
+    # pred = model.predict(X.reshape(1, -1))
+    # print(A.shape)
+    A = A.values.T
+    B = B.values.T
+    A = pr_scaler.transform(A[:,-30:].reshape(-1)[None,:])
+    B = vo_scaler.transform(B[:,-30:].reshape(-1)[None,:])
+    input = np.concatenate((A,B),axis = 1)
+    input = torch.FloatTensor(input)
+    output = model.encoder(input[None, :])
+    # print(output.detach().numpy().shape)
+    pred = reg.predict(output.detach().numpy()[0])
+    # print(pred.shape)
+    return pred - A[:,-1]
+    # return np.array([m.predict(input)[0] for m in model]) - A[:,-1]
